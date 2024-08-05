9830a1bd8f4e3cd98dc52e5f334ddeaf7661069a
m4ndsr <sybren.it@gmail.com>
2024-08-05 19:56:10 +0200

    Added consumer plugin

--- ConsumerPlugin/src/main/java/me/m4ndsr/consumerplugin/IronOreBrokenEventListener.java
+++ ConsumerPlugin/src/main/java/me/m4ndsr/consumerplugin/IronOreBrokenEventListener.java
@@ -1,13 +1,20 @@
 package me.m4ndsr.consumerplugin;
 
+import me.m4ndsr.providerplugin.IronOreBrokenEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 
 public class IronOreBrokenEventListener implements Listener {
 
     @EventHandler
     public void onIronBreak(IronOreBrokenEvent e) {
         System.out.println(e.getPlayer().getName() + " broke an iron ore");
+    }
+
+    @EventHandler
+    public void onBreak(BlockBreakEvent e) {
+        System.out.println("listener is registered");
     }
 
 }
